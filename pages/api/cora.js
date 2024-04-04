7e1cdfa6e147a869e237c2f94548f9eef2ebf446
Caio Brito <caio.freitas.brito@gmail.com>
2024-04-04 02:09:25 -0300

    Other resource - API

--- pages/api/cora.js
+++ pages/api/cora.js
@@ -1,13 +1,13 @@
 function coraToken(request, response) {
 
-  const apiSecret = process.env.API_SECRET
+  const apiSecret = process.env.SECRET_KEY
   const dynamicDate = new Date().toDateString();
 
   response.json({
     time: dynamicDate,
     secret: apiSecret
   });
-  
+
 }
 
 export default coraToken;
