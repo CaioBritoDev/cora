fcde70f123edc23ee894121f3b08a52ed26be314
Caio Brito <caio.freitas.brito@gmail.com>
2024-04-04 03:06:53 -0300

    With Axios

--- pages/api/cora.js
+++ pages/api/cora.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 async function coraToken(request, response) {
-
   const apiSecret = process.env.CLIENT_ID_STAGE; // In vercel, no possible to access in localhost
 
   const cert = "./keys/certificate.pem";
@@ -9,7 +8,24 @@ async function coraToken(request, response) {
   const url = "https://matls-clients.api.stage.cora.com.br/token";
   const params = `grant_type=client_credentials&client_id=${apiSecret}`;
 
-  axios.post(url, params, {
+  var xhr = new XMLHttpRequest();
+
+  xhr.open("POST", url, true);
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.setRequestHeader("cert", cert);
+  xhr.setRequestHeader("key", key);
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+      response.json({
+        response: xhr.responseText,
+      });
+    }
+  };
+
+  xhr.send(params);
+
+  /*axios.post(url, params, {
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
         cert: cert,
@@ -21,11 +37,10 @@ async function coraToken(request, response) {
     .catch((error) => {
       console.error("Erro ao fazer a solicitação:", error);
       response.json(error.response);
-    });
+    });*/
 
   // If you want to save the response in cache and perform your endpoint - in 10 seconds, versel make other request and put in the cache - your website never going to crash -> just for generic responses endpoints. Not my case
   // response.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate')
-
 }
 
 export default coraToken;
