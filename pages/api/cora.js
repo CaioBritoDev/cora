2453eba65c775c7ac10684d3fc5223d152b58b01
Caio Brito <caio.freitas.brito@gmail.com>
2024-04-04 11:31:47 -0300

    With http agent

--- pages/api/cora.js
+++ pages/api/cora.js
@@ -1,43 +1,45 @@
 import axios from "axios";
+const https = require("https");
+const fs = require("fs");
 
 async function coraToken(request, response) {
   const apiSecret = process.env.CLIENT_ID_STAGE; // In vercel, no possible to access in localhost
 
-  const cert = "./keys/certificate.pem";
-  const key = "./keys/private-key.key";
+  // const cert = "./keys/certificate.pem";
+  // const key = "./keys/private-key.key";
+  // const url = "https://matls-clients.api.stage.cora.com.br/token";
+
+  
+  const cert = fs.readFileSync(
+    "./keys/certificate.pem"
+  );
+  const key = fs.readFileSync(
+    "./keys/private-key.key"
+  );
   const url = "https://matls-clients.api.stage.cora.com.br/token";
-  const params = `grant_type=client_credentials&client_id=${apiSecret}`;
+  const params = new URLSearchParams({
+    grant_type: "client_credentials",
+    client_id: apiSecret,
+  }).toString();
 
-  var xhr = new XMLHttpRequest();
+  const agent = new https.Agent({
+    cert: cert,
+    key: key,
+  });
 
-  xhr.open("POST", url, true);
-  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-  xhr.setRequestHeader("cert", cert);
-  xhr.setRequestHeader("key", key);
-
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState == XMLHttpRequest.DONE) {
-      response.json({
-        response: xhr.responseText,
-      });
-    }
-  };
-
-  xhr.send(params);
-
-  /*axios.post(url, params, {
+  axios
+    .post(url, params, {
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
-        cert: cert,
-        key: key,
-      }
+      },
+      httpsAgent: agent,
     })
-    .then((res) => res.json())
-    .then((jsonFormat) => response.json(jsonFormat))
+    .then((res) => {
+      response.json(res.data)
+    })
     .catch((error) => {
-      console.error("Erro ao fazer a solicitação:", error);
-      response.json(error.response);
-    });*/
+      response.json(error)
+    });
 
   // If you want to save the response in cache and perform your endpoint - in 10 seconds, versel make other request and put in the cache - your website never going to crash -> just for generic responses endpoints. Not my case
   // response.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate')
